f0595595ed77037d44b41f6bebfe2f600e702230
unknown <brandonluu9@gmail.com>
2025-08-17 11:31:28 -0700

    adding more layers to model

--- model/model.py
+++ model/model.py
@@ -4,12 +4,23 @@ import torch
 import torch.nn as nn
 
 class model(nn.Module):
-    def __init__(self):
+    def __init__(self, dimension=64, numFeatures=12):
         super().__init__()
 
         # Embedding layers:
-        embedding = nn.Embedding(1000, 64)  # Example embedding layer
-    
+        self.embedding = nn.Embedding(65023, dimension)  # Example embedding layer
+        self.embeddingSire = nn.Embedding(1030, dimension)
+        self.embeddingDam = nn.Embedding(27100, dimension)
+        self.embeddingBmSire = nn.Embedding(2091, dimension)
+
+        # Dropout layer and normalization layer:
+        self.dropout = nn.Dropout(0.25)
+        self.norm = nn.LayerNorm(dimension * 4)
+
+        self.batchNorm = nn.BatchNorm1d(numFeatures)
+
+
+
 
     def forward(self, x):
-    
+        return
